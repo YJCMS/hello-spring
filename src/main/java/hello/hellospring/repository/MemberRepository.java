012f1aa96b169c05ae95af89bd101d9d46f36f37
YJCMS <sen9563@gmail.com>
2022-04-21 16:27:56 +0900

    컴포넌트 스캔, 스프링 빈 등록, 회원 등록 조회

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,9 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public interface MemberRepository {
     Member save(Member member);
